a4085d1574ae7079740e5db969f327f8b7215575
Endorfin86 <yae.rodionov@gmail.com>
2023-04-30 11:33:49 +0300

    change on webhook

--- bot.py
+++ bot.py
@@ -1,10 +1,26 @@
 from aiogram import executor
 from create_bot import dp
+from config import URL_APP
+from create_bot import bot
+import os
 
-async def on_startup(_):
+async def on_startup(dp):
+    await bot.set_webhook(URL_APP)
     print('Бот запущен')
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
 
 from handlers import client
 client.register_handlers_client(dp)
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+executor.start_webhook(
+    dispatcher=dp,
+    webhook_path='',
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host="0.0.0.0",
+    port=4000
+    )
